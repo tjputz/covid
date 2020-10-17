3c9dca59a0e943c3b1bacfc3cd53f79ec05c1394
Tim Putz <tjputz@hotmail.com>
2020-10-17 09:55:03 -0400

    Fixed layout overlap Added submit button Added text notifiction Fixed code after error all works again HTTP Code

--- app/src/main/java/com/imperialrevoltsoftware/covid_19/MainActivity.java
+++ app/src/main/java/com/imperialrevoltsoftware/covid_19/MainActivity.java
@@ -1,7 +1,6 @@
 package com.imperialrevoltsoftware.covid_19;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.util.Log;
@@ -9,28 +8,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
-
 import com.android.volley.*;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-
 import org.json.JSONException;
 import org.json.JSONObject;
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-
-import javax.net.ssl.HttpsURLConnection;
+import org.json.JSONArray;
 
 
 public class MainActivity extends AppCompatActivity  {
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,13 +25,12 @@ public class MainActivity extends AppCompatActivity  {
 
         final Button submit = findViewById(R.id.submit);
         final SmsManager smgr = SmsManager.getDefault();
-
-
-
-
         final RequestQueue queue = Volley.newRequestQueue(this);
-        String VAurl ="https://api.covidtracking.com/v1/states/va/current.json";
 
+
+        String VAurl ="https://api.covidtracking.com/v1/states/va/current.json";
+        String USurl = "https://api.covidtracking.com/v1/us/current.json";
+        String Worldurl = "https://api.covid19api.com/summary";
 
 
         // Request a string response from the provided URL.
@@ -95,9 +80,136 @@ public class MainActivity extends AppCompatActivity  {
             public void onErrorResponse(VolleyError error) {
                 //textView.setText("That didn't work!");
             }
-        });
-
+        }
+        );
         queue.add(VAstringRequest);
+
+        // Request a string response from the provided URL.
+        StringRequest USstringRequest = new StringRequest(Request.Method.GET, USurl,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                       JSONArray jsonarray = null;
+
+                       try {
+
+                             jsonarray = new JSONArray(response);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+                        try {
+                            int UStotalPositiveCases = 0;
+                            int UStotalPositiveCasesIncrease = 0;
+
+                            for(int i=0; i<jsonarray.length(); i++){
+
+                                JSONObject obj = jsonarray.getJSONObject(i);
+                                UStotalPositiveCases = obj.getInt("positive");
+                                UStotalPositiveCasesIncrease = obj.getInt("positiveIncrease");
+
+                                }
+
+                            //US Information
+                            //US Total Positive Cases
+
+                            TextView USPositiveCases = findViewById(R.id.USpositiveCases);
+                            USPositiveCases.setText("Total Positive Cases: " + UStotalPositiveCases);
+
+                            //US Total Positive Cases Increase
+
+                            TextView USPositiveCasesIncrease = findViewById(R.id.USpositiveCasesIncrease);
+                            USPositiveCasesIncrease.setText("Total Positive Increase: " + UStotalPositiveCasesIncrease);
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+                        // Display the first 500 characters of the response string.
+                        //textView.setText("Response is: "+ response.substring(0,500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //textView.setText("That didn't work!");
+            }
+        }
+        );
+
+        queue.add(USstringRequest);
+
+        // Request a string response from the provided URL.
+        StringRequest WorldstringRequest = new StringRequest(Request.Method.GET, Worldurl,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        JSONObject jsonobject = null;
+
+                        try {
+
+                            jsonobject = new JSONObject(response);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+                        try {
+                            int WorldtotalPositiveCases = 0;
+                            int WorldtotalPositiveCasesIncrease = 0;
+
+                            //for(int i=0; i<jsonarray.length(); i++){
+
+                                //JSONObject obj = jsonarray.getJSONObject(i);
+                                //WorldtotalPositiveCases = obj.getInt("positive");
+                                //WorldtotalPositiveCasesIncrease = obj.getInt("positiveIncrease");
+
+                            //}
+                            WorldtotalPositiveCases = jsonobject.getJSONObject("Global").getInt("TotalConfirmed");
+                            WorldtotalPositiveCasesIncrease = jsonobject.getJSONObject("Global").getInt("NewConfirmed");
+
+                            //Log.d("totalconfirmed", String.valueOf(WorldtotalPositive));
+                            //Log.d("newconfirmed", String.valueOf(WorldtotalPositiveCasesIncrease));
+
+
+                            //US Information
+                            //US Total Positive Cases
+
+                            TextView WorldPositiveCases = findViewById(R.id.worldCovidCasesNumber);
+                            String worldtotalPositiveCases = String.valueOf(WorldtotalPositiveCases);
+                            WorldPositiveCases.setText("Total Positive Cases: " + WorldtotalPositiveCases);
+
+                            //US Total Positive Cases Increase
+
+                            TextView WorldPositiveCasesIncrease = findViewById(R.id.worldCovidCasesIncrease);
+                            String worldtotalPositiveCasesIncrease = String.valueOf(WorldtotalPositiveCasesIncrease);
+                            WorldPositiveCasesIncrease.setText("Total Positive Increase: " + WorldtotalPositiveCasesIncrease);
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+                        // Display the first 500 characters of the response string.
+                        //textView.setText("Response is: "+ response.substring(0,500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //textView.setText("That didn't work!");
+            }
+        }
+        );
+
+        queue.add(WorldstringRequest);
+
+
 
         submit.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
@@ -133,4 +245,5 @@ public class MainActivity extends AppCompatActivity  {
         });
 
 
-        }}
+        }
+        }
