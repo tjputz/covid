394672234cdc199eb7e8a3f74571d30c36573b42
Tim Putz <tjputz@hotmail.com>
2020-10-20 20:01:07 -0400

    Fixed layout overlap Added submit button Added text notifiction Fixed code after error all works again HTTP Code World Numbers No SMS or Internet Permissions

--- app/src/main/java/com/imperialrevoltsoftware/covid_19/MainActivity.java
+++ app/src/main/java/com/imperialrevoltsoftware/covid_19/MainActivity.java
@@ -195,38 +195,35 @@ public class MainActivity extends AppCompatActivity  {
 
 
 
-        submit.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
+            submit.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View view) {
 
 
+                    final CheckBox checkBox0 = findViewById(R.id.checkBox0);
+                    final CheckBox checkBox1 = findViewById(R.id.checkBox1);
+                    final CheckBox checkBox2 = findViewById(R.id.checkBox2);
+                    final CheckBox checkBox3 = findViewById(R.id.checkBox3);
+                    final CheckBox checkBox4 = findViewById(R.id.checkBox4);
+                    final CheckBox checkBox5 = findViewById(R.id.checkBox5);
+                    final CheckBox checkBox6 = findViewById(R.id.checkBox6);
+                    final CheckBox checkBox7 = findViewById(R.id.checkBox7);
+                    final CheckBox checkBox8 = findViewById(R.id.checkBox8);
 
-                final CheckBox checkBox0 = findViewById(R.id.checkBox0);
-                final CheckBox checkBox1 = findViewById(R.id.checkBox1);
-                final CheckBox checkBox2 = findViewById(R.id.checkBox2);
-                final CheckBox checkBox3 = findViewById(R.id.checkBox3);
-                final CheckBox checkBox4 = findViewById(R.id.checkBox4);
-                final CheckBox checkBox5 = findViewById(R.id.checkBox5);
-                final CheckBox checkBox6 = findViewById(R.id.checkBox6);
-                final CheckBox checkBox7 = findViewById(R.id.checkBox7);
-                final CheckBox checkBox8 = findViewById(R.id.checkBox8);
-
-                checkBox0.setChecked(false);
-                checkBox1.setChecked(false);
-                checkBox2.setChecked(false);
-                checkBox3.setChecked(false);
-                checkBox4.setChecked(false);
-                checkBox5.setChecked(false);
-                checkBox6.setChecked(false);
-                checkBox7.setChecked(false);
-                checkBox8.setChecked(false);
-
+                    checkBox0.setChecked(false);
+                    checkBox1.setChecked(false);
+                    checkBox2.setChecked(false);
+                    checkBox3.setChecked(false);
+                    checkBox4.setChecked(false);
+                    checkBox5.setChecked(false);
+                    checkBox6.setChecked(false);
+                    checkBox7.setChecked(false);
+                    checkBox8.setChecked(false);
 
 
-            }
+                }
 
 
-
-        });
+            });
 
 
         }
